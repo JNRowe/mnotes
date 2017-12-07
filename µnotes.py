1172a634cbca3522eef204b506e2f5397b4a3575
James Rowe <jnrowe@gmail.com>
2017-12-07 17:40:14 +0000

    Add abbreviation expansion support

--- µnotes.py
+++ µnotes.py
@@ -6,6 +6,12 @@ import re
 import jinja2
 
 
+with open('data/abbrevs.dat') as f:
+    ABBREVS = [l.strip() for l in f]
+_ABBRREVISE = lambda s: ''.join([s[0] for s in s.split()])  # NOQA
+ABBREVS = {re.compile(fr'\b{_ABBRREVISE(s)}\b'):
+           f'<abbr title="{s}">{_ABBRREVISE(s)}</abbr>'
+           for s in ABBREVS}
 ENV = jinja2.Environment(loader=jinja2.FileSystemLoader('templates/'))
 HTML_FILTERS = {
     re.compile(r'(#\w+)'): r'<b>\1</b>',
@@ -18,6 +24,8 @@ HTML_FILTERS = {
 def htmlise(dct):
     if 'text' in dct:
         for pat, repl in HTML_FILTERS.items():
+            dct['text'] = pat.sub(repl, dct['text'])
+        for pat, repl in ABBREVS.items():
             dct['text'] = pat.sub(repl, dct['text'])
     return dct
 
