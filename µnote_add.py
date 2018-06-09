7d1d6b8e5d1af3d0ca6240506d7a2f18cc1e5485
James Rowe <jnrowe@gmail.com>
2018-06-09 01:52:17 +0100

    Add page reply support

--- µnote_add.py
+++ µnote_add.py
@@ -12,21 +12,32 @@ except FileNotFoundError:
     notes = []
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--reply-to')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-u', '--reply-url')
+parser.add_argument('-U', '--reply-title')
+group.add_argument('-r', '--reply-to')
 parser.add_argument('-t', '--reply-quote')
 parser.add_argument('-i', '--reply-time')
 parser.add_argument('text')
 args = parser.parse_args()
-if any([args.reply_to, args.reply_quote, args.reply_time]) and not \
+if any([args.reply_url, args.reply_title]) and not \
+   all([args.reply_url, args.reply_title]):
+    raise argparse.ArgumentTypeError('URL replies require -u and -U')
+elif any([args.reply_to, args.reply_quote, args.reply_time]) and not \
    all([args.reply_to, args.reply_quote, args.reply_time]):
-    raise argparse.ArgumentTypeError('Replies require -r, -t and -i')
+    raise argparse.ArgumentTypeError('Toot replies require -r, -t and -i')
 
 ts = subprocess.check_output(['date', '-Iseconds']).decode().strip()
 note = {
     'text': args.text,
     'timestamp': ts,
 }
-if args.reply_to:
+if args.reply_url:
+    note['in_reply_url'] = {
+        'title': args.reply_title,
+        'url': args.reply_url
+    }
+elif args.reply_to:
     note['in_reply_to'] = {
         'text': args.reply_quote,
         'toot': {
