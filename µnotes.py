4c589991ce9434bc4b8732431f4d91f29ce6b36c
James Rowe <jnrowe@gmail.com>
2018-09-21 07:45:36 +0100

    Add “*<word>*” formatting support

--- µnotes.py
+++ µnotes.py
@@ -19,6 +19,7 @@ HTML_FILTERS = {
     re.compile(r'(https?://[\w\.?=\+/_-]+)', re.IGNORECASE):
         r'<a href="\1">\1</a>',
     re.compile(r'(@\w+(?:@\w+)*)'): r'<em>\1</em>',
+    re.compile(r'\*(\w+)\*'): r'<strong>\1</strong>',
 }
 
 
