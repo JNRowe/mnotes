9651dc3360b8066fe9ffc40b67614ae9b5211989
James Rowe <jnrowe@gmail.com>
2018-10-20 14:47:48 +0100

    Use AttrDict for JSON access

--- µnotes_twtxt.py
+++ µnotes_twtxt.py
@@ -4,16 +4,18 @@ import json
 import re
 import textwrap
 
+from jnrbase.attrdict import AttrDict
+
 
 with open('data/notes.json') as f:
-    notes = json.load(f)
+    notes = json.load(f, object_hook=AttrDict)
 
 
 for note in notes:
-    note['text'] = re.sub(r'@([@\w]+)@(\w+)',
-                          r'@<\1 https://mastodon.\2/\1/twtxt.txt>',
-                          note['text'])
+    note.text = re.sub(r'@([@\w]+)@(\w+)',
+                       r'@<\1 https://mastodon.\2/\1/twtxt.txt>',
+                       note.text)
     first = True
-    for chunk in textwrap.wrap(note['text'], 140):
-        print(f'{note["timestamp"]}	{"" if first else "…"}{chunk}')
+    for chunk in textwrap.wrap(note.text, 140):
+        print(f'{note.timestamp}	{"" if first else "…"}{chunk}')
         first = False
