642664fca95303a063ae301604bb1d2a986939f8
James Rowe <jnrowe@gmail.com>
2018-11-20 14:20:16 +0000

    Don’t break twtxt output mid user reference

--- µnotes_twtxt.py
+++ µnotes_twtxt.py
@@ -13,9 +13,10 @@ with open('data/notes.json') as f:
 
 for note in notes:
     note.text = re.sub(r'@([@\w]+)@(\w+)',
-                       r'@<\1 https://mastodon.\2/\1/twtxt.txt>',
+                       '@<\\1\N{US}https://mastodon.\\2/\\1/twtxt.txt>',
                        note.text)
     first = True
     for chunk in textwrap.wrap(note.text, 140):
+        chunk.replace('\N{US}', ' ')
         print(f'{note.timestamp}	{"" if first else "…"}{chunk}')
         first = False
